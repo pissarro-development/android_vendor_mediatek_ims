ea3b3ca2b663f57bbeda04e3be2137251da772b1
z3rokwq <z3rokwq@gmail.com>
2026-01-23 21:35:44 +0300

    ims: Provide MTK IMS sysconfig
    * This add the IMS package into the whitelist for ignoring location settings (emergency purposes).
    
    Signed-off-by: z3rokwq <z3rokwq@gmail.com>

--- ims.mk
+++ ims.mk
@@ -29,3 +29,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+
+# SysConfigs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfigs/com.mediatek.ims.config.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.mediatek.ims.config.xml
